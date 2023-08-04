bc0c7f973781f41bb988624f81b80ea33d956cb0
Gagan Vishal Mishra <gagan.vishal@mmltd.co.za>
2023-08-04 15:50:57 +0200

    Initial Commit

---
+++ CarouselViewSwiftUI/CarouselViewSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarouselViewSwiftUIApp.swift
+//  CarouselViewSwiftUI
+//
+//  Created by Gagan Vishal  on 2023/08/04.
+//
+
+import SwiftUI
+
+@main
+struct CarouselViewSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
